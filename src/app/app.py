fd11a5dfce566a0d9cd4e991351caa1245e0553e
Andy Watson <andy.watson@ignitionone.com>
2018-11-06 12:21:10 -0500

    generated route templates

--- src/app/app.py
+++ src/app/app.py
@@ -8,37 +8,37 @@ def home():
     return render_template("index.html", name='home')
 
 
-## viz pages
-# @app.route("/")
-# def home():
-#     return render_template("index.html", name='home')
+# viz pages
+@app.route("/fig/max_temp")
+def max_temp():
+    return render_template("max_temp.html", name='max_temp')
 
 
-# @app.route("/")
-# def home():
-#     return render_template("index.html", name='home')
+@app.route("/fig/humidity")
+def humidity():
+    return render_template("humidity.html", name='humidity')
 
 
-# @app.route("/")
-# def home():
-#     return render_template("index.html", name='home')
+@app.route("/fig/cloud_cover")
+def cloud_cover():
+    return render_template("cloud_cover.html", name='cloud_cover')
 
 
-# @app.route("/")
-# def home():
-#     return render_template("index.html", name='home')
+@app.route("/fig/wind_speed")
+def wind_speed():
+    return render_template("wind_speed.html", name='wind_speed')
 
 
-## comparison page
-# @app.route("/")
-# def home():
-#     return render_template("index.html", name='home')
+# comparison page
+@app.route("/comparisons")
+def comparisons():
+    return render_template("comparisons.html", name='comparisons')
 
 
-## data page
-# @app.route("/")
-# def home():
-#     return render_template("index.html", name='home')
+# data page
+@app.route("/data")
+def data():
+    return render_template("data.html", name='data')
 
 
 if __name__ == "__main__":
